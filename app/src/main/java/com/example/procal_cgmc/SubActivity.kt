fecc84fea262b6bf41ed5c514bb50fe236876ee5
jiwon <leejw9906@naver.com>
2022-11-25 13:34:13 +0900

    진수 변환 기능 구현, 비트 연산 UI

--- app/src/main/java/com/example/procal_cgmc/SubActivity.kt
+++ app/src/main/java/com/example/procal_cgmc/SubActivity.kt
@@ -1,56 +1,56 @@
 package com.example.procal_cgmc
 
-import android.content.Intent
 import android.os.Bundle
 import android.view.View
-import android.widget.Button
+import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import com.example.procal_cgmc.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity() {
+class SubActivity : AppCompatActivity() {
     var isFirstInput = true // 첫 번째 입력값인지 확인
     var isOperatorClick = false // 연산자가 클릭이 되어 있는지 체크
     var resultNumber = 0.0
     var inputNumber = 0.0
     var operator = "="
     var lastOperator = "+"
-    var activityMainBinding: ActivityMainBinding? = null
+    var resultTextView: TextView? = null
+    var mathTextView : TextView? = null
+    var radix2Num: TextView? = null
+    var radix8Num: TextView? = null
+    var radix16Num: TextView? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        activityMainBinding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(R.layout.activity_main)
-        setContentView(activityMainBinding!!.root)
+        setContentView(R.layout.subactivity_main)
 
-        val imageButton = findViewById<View>(R.id.button4) as Button
-        imageButton.setOnClickListener { view: View? ->
-            val intent = Intent(applicationContext, SubActivity::class.java)
-            startActivity(intent)
-        }
+        resultTextView = findViewById<View>(R.id.resultTextView) as TextView
+        mathTextView = findViewById<View>(R.id.mathTextView) as TextView
+        radix2Num = findViewById<View>(R.id.radix2Num) as TextView
+        radix8Num = findViewById<View>(R.id.radix8Num) as TextView
+        radix16Num = findViewById<View>(R.id.radix16Num) as TextView
     }
 
     fun numButtonClick(view: View) {
         if (isFirstInput) {
-            activityMainBinding?.resultTextView?.text = view.tag.toString()
+            resultTextView?.text = view.tag.toString()
             isFirstInput = false
             if (operator == "=") {
-                activityMainBinding?.mathTextView?.text = null
+                mathTextView?.text = null
                 isOperatorClick = false
             }
         } else {
-            if (activityMainBinding?.resultTextView?.text.toString() == "0") {
+            if (resultTextView?.text.toString() == "0") {
                 Toast.makeText(this, "0으로 시작되는 숫자는 없습니다.", Toast.LENGTH_SHORT).show()
                 isFirstInput = true
             } else {
-                activityMainBinding?.resultTextView?.append(view.tag.toString())
+                resultTextView?.append(view.tag.toString())
             }
         }
     }
 
     fun allClearButtonClick(view: View?) {
-        activityMainBinding?.resultTextView?.text = "0"
-        activityMainBinding?.mathTextView?.text = ""
+        resultTextView?.text = "0"
+        mathTextView?.text = ""
         resultNumber = 0.0
         operator = "="
         isFirstInput = true
@@ -63,51 +63,53 @@ class MainActivity : AppCompatActivity() {
         if (isFirstInput) { // 서로 다른 연산자를 연속으로 입력시 연산자 변경
             if (operator == "=") { // =이 눌리면 mathText 연살 결과 값으로 변경
                 operator = view.tag.toString()
-                resultNumber = activityMainBinding?.resultTextView?.text.toString().toDouble()
-                activityMainBinding?.mathTextView?.text = "$resultNumber $operator "
+                resultNumber = resultTextView?.text.toString().toDouble()
+                mathTextView?.text = "$resultNumber $operator "
             } else {
                 operator = view.tag.toString()
-                val getMathText: String = activityMainBinding?.mathTextView?.text.toString()
+                val getMathText: String = mathTextView?.text.toString()
                 val subString = getMathText.substring(0, getMathText.length - 2) // 공백 + 연산자
-                activityMainBinding?.mathTextView?.text = subString
-                activityMainBinding?.mathTextView?.append("$operator ")
+                mathTextView?.text = subString
+                mathTextView?.append("$operator ")
             }
         } else {
-            inputNumber = activityMainBinding?.resultTextView?.text.toString().toDouble()
+            inputNumber = resultTextView?.text.toString().toDouble()
             resultNumber = calculator(resultNumber, inputNumber, operator)
-            activityMainBinding?.resultTextView?.text = resultNumber.toString() // operator 넣기
+            resultTextView?.text = resultNumber.toString() // operator 넣기
             isFirstInput = true
             operator = view.tag.toString()
-            activityMainBinding?.mathTextView?.append("$inputNumber $operator") // mathText에 출력
+            mathTextView?.append("$inputNumber $operator") // mathText에 출력
         }
     }
 
     fun equalsButtonClick(view: View) {
         if (isFirstInput) { // =이 두번 입력되었을 때
             if (isOperatorClick) {
-                activityMainBinding?.mathTextView?.text = "$resultNumber $lastOperator $inputNumber "
+                mathTextView?.text = "$resultNumber $lastOperator $inputNumber "
                 resultNumber = calculator(resultNumber, inputNumber, lastOperator)
-                activityMainBinding?.resultTextView?.text = resultNumber.toString()
+                resultTextView?.text = resultNumber.toString()
+                radixTransformation()
             }
         } else { // =이 한번일 때
-            inputNumber = activityMainBinding?.resultTextView?.text.toString().toDouble()
+            inputNumber = resultTextView?.text.toString().toDouble()
             resultNumber = calculator(resultNumber, inputNumber, operator)
-            activityMainBinding?.resultTextView?.text = resultNumber.toString() // operator 넣기
+            resultTextView?.text = resultNumber.toString() // operator 넣기
             isFirstInput = true
             operator = view.tag.toString()
-            activityMainBinding?.mathTextView?.append("$inputNumber $operator") // mathText에 출력
+            mathTextView?.append("$inputNumber $operator") // mathText에 출력
+            radixTransformation()
         }
     }
 
     fun pointButtonClick(view: View) {
         if (isFirstInput) {
-            activityMainBinding?.resultTextView?.text = "0" + view.tag.toString()
+            resultTextView?.text = "0" + view.tag.toString()
             isFirstInput = false
         } else {
-            if (activityMainBinding?.resultTextView?.text.toString().contains(".")) {
+            if (resultTextView?.text.toString().contains(".")) {
                 Toast.makeText(this, "이미 소숫점이 존재합니다.", Toast.LENGTH_SHORT).show()
             } else {
-                activityMainBinding?.resultTextView?.append(view.tag.toString())
+                resultTextView?.append(view.tag.toString())
             }
         }
     }
@@ -126,14 +128,37 @@ class MainActivity : AppCompatActivity() {
 
     fun backspaceButtonClick(view: View?) {
         if (!isFirstInput) {
-            val getResultText: String = activityMainBinding?.resultTextView?.text.toString()
+            val getResultText: String = resultTextView?.text.toString()
             if (getResultText.length > 1) {
                 val subString = getResultText.substring(0, getResultText.length - 1)
-                activityMainBinding?.resultTextView?.text = subString
+                resultTextView?.text = subString
             } else {
-                activityMainBinding?.resultTextView?.text = "0"
+                resultTextView?.text = "0"
                 isFirstInput = true
             }
         }
+    }
+
+    private fun radixTransformation() {
+        val strnum = resultTextView!!.text.toString()
+        var num = -1
+        if(strnum.indexOf('.') + 2 == strnum.length && strnum.get(strnum.length - 1) == '0') {
+            num = strnum.toFloat().toInt()
+        } else {
+            Toast.makeText(this, "정수만 진수 변환이 가능합니다.", Toast.LENGTH_SHORT).show()
+            radix2Num!!.text = ""
+            radix8Num!!.text = ""
+            radix16Num!!.text = ""
+            return
+        }
+
+            val str2num = Integer.toBinaryString(num) //10진수 -> 2진수
+            radix2Num!!.text = str2num
+
+            val str8num = Integer.toOctalString(num) //10진수 -> 8진수
+            radix8Num!!.text = str8num
+
+            val str16num = Integer.toHexString(num) //10진수 -> 16진수
+            radix16Num!!.text = str16num
     }
 }
